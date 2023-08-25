364d566bfeca96490bc31f4df6b7f66db3adca27
JoseA27 <josrobles@estudiantec.cr>
2023-08-25 17:19:02 -0600

    Orden Alfabético Correción de Añadir

--- AWSDB/AWSDB/Controllers/HomeController.cs
+++ AWSDB/AWSDB/Controllers/HomeController.cs
@@ -1,43 +1,43 @@
 ﻿using AWSDB.Data;
 using AWSDB.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 using System.Diagnostics;
-using System.Reflection.Metadata;
 
 namespace AWSDB.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDBContext _db;
-        public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
-        {
-            _db = db;
-            _logger = logger;
-        }
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDBContext _db;
+		string connetionString;
+		public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
+		{
+			_db = db;
+			_logger = logger;
+			connetionString = "server=tecdb.ctfxom3mv69f.us-east-2.rds.amazonaws.com,1433;Database=WebLeads;TrustServerCertificate=True;User ID=admin;Password=tecaws123;";
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
-        //---------------------------------------------------------------------------------------------------------
+		//---------------------------------------------------------------------------------------------------------
 
-        public IActionResult Index()
-        {
-            var getArticulo = _db.Articulo.FromSqlRaw("getArticulo").ToList();
-            return View(getArticulo);
-        }
+		public IActionResult Index()
+		{
+			var getArticulo = _db.Articulo.FromSqlRaw("getArticulo").ToList();
+			return View(getArticulo);
+		}
 
 		public IActionResult Volver()
 		{
@@ -45,7 +45,7 @@ namespace AWSDB.Controllers
 		}
 		public IActionResult Create()
 		{
-            return View();
+			return View();
 		}
 		public IActionResult CreateArticle()
 		{
@@ -55,35 +55,34 @@ namespace AWSDB.Controllers
 
 		public IActionResult actualizar()
 		{
+			
+			using (SqlConnection connection = new SqlConnection(connetionString))
+			{
+				connection.Open();
+				using (SqlCommand command = new SqlCommand("AddArticulo", connection))
+				{
+					command.CommandType = CommandType.StoredProcedure;
 
-			string connetionString = null;
-			SqlConnection connection;
-			SqlDataAdapter adapter;
-			SqlCommand command = new SqlCommand();
-			SqlParameter param, param2, param3;
-			DataSet ds = new DataSet();
-			connetionString = "server = tecdb.ctfxom3mv69f.us-east-2.rds.amazonaws.com,1433;Database=WebLeads;TrustServerCertificate=True;User ID=admin;Password=tecaws123;";
-			connection = new SqlConnection(connetionString);
-			connection.Open();
-			command.Connection = connection;
-			command.CommandType = CommandType.StoredProcedure;
-			command.CommandText = "AddArticulo";
-			param = new SqlParameter("@inNombre", "Teja");
-			param2 = new SqlParameter("@inPrecio", "1000");
-			param3 = new SqlParameter("@outResultCode", 0);
-			param.Direction = ParameterDirection.Input;
-			param2.Direction = ParameterDirection.Input;
-			param3.Direction = ParameterDirection.Output;
-			param.DbType = DbType.String;
-			param2.DbType = DbType.Currency;
-			param3.DbType = DbType.Int32;
-			command.Parameters.Add(param);
-			command.Parameters.Add(param2);
-			command.Parameters.Add(param3);
-			adapter = new SqlDataAdapter(command);
-			adapter.Fill(ds);
-			connection.Close();
-			return RedirectToAction("Index", "Home");
+					command.Parameters.AddWithValue("@inNombre", "Lata Zinc");
+					command.Parameters.AddWithValue("@inPrecio", 4000);
+					command.Parameters.Add("@outResultCode", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+					command.ExecuteNonQuery();
+
+					int resultCode = Convert.ToInt32(command.Parameters["@outResultCode"].Value);
+					connection.Close();
+					if (resultCode==50002)
+					{
+						return RedirectToAction("Create", "Home");
+					}
+					else
+					{
+						return RedirectToAction("Index", "Home");
+					}
+				}
+			}
 		}
+
+		
 	}
 }
